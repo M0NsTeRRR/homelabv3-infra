43c4c64f21c2b42367bb38953b6755d14b252069
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-01-21 14:01:37 +0000

    fix: devcontainer variable export

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,12 +4,6 @@
 sudo apt update
 sudo apt install -y libcairo2 libcairo2-dev
 
-# Set kubeconfig in bashrc
-echo 'export KUBECONFIG=$(for YAML in $(find ${HOME}/.kube -maxdepth 1 -type f -name '\''*'\'') ; do echo -n ":${YAML}"; done)' >> /home/vscode/.zshrc
-
-# Set REQUESTS_CA_BUNDLE for python script
-echo 'export REQUESTS_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"' >> /home/vscode/.zshrc
-
 # install PKI
 sudo cp ssl/*.crt /usr/local/share/ca-certificates
 sudo update-ca-certificates
